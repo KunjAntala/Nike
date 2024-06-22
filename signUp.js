ee6e3dc2d2985b99291ea7983e4a30345d5dfdc4
KunjGit1 <138096211+KunjGit1@users.noreply.github.com>
2024-06-21 22:36:28 -0700

    new change

--- signUp.js
+++ signUp.js
@@ -1,3 +1,50 @@
+function validateForm(){
+
+    var first_name = document.myform.first_name.value;
+    var last_name = document.myform.last_name.value;
+    var email = document.myform.email.value;
+    var password = document.myform.password.value;
+    var phone_number = document.myform.phone_number.value;
+    var address = document.myform.address.value;
+
+    if (first_name == null || first_name == ""){ 
+        document.getElementById("error").innerHTML = "First Name is Required*";
+        return false;
+    } 
+    else if (last_name == null || last_name == "") {
+        document.getElementById("errorr").innerHTML = "Last Name is Required*";
+        return false;
+    }
+    else if (email == null || email == "") {
+        document.getElementById("errorrr").innerHTML = "Email is Required*";
+        return false;
+    }
+    else if (password == null || password == "") {
+        document.getElementById("errorrrr").innerHTML = "Password is Required*";
+        return false;
+    }
+    else if (phone_number == null || phone_number == "") {
+        document.getElementById("errorrrrr").innerHTML = "Phone Number is Required*";
+        return false;
+    }
+    else if (address == null || address == "") {
+        document.getElementById("errorrrrrr").innerHTML = "Address is Required*";
+        return false;
+    }
+    else if (pass.length < 6) {
+        document.getElementById("errorrrr").innerHTML = "**Password length must be atleast 8 characters";
+        return false;
+    }
+    if (pass.length > 15) {
+        document.getElementById("errorrrr").innerHTML = "**Password length must not exceed 15 characters";
+        return false;
+    } 
+    else {
+        return true;
+    }
+    
+}
+
 function saveData() {
     let first_name,last_name,email,password,phone_number,address;
 
@@ -13,7 +60,8 @@ function saveData() {
 
     if (user_records.some((v) => {return v.email == email})){
         alert(email + "is already register.")
-    }else{
+    }
+    else{
         user_records.push({
             "first_name": first_name,
             "last_name": last_name,
